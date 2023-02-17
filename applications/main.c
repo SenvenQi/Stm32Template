70552d64ec43720b76a39b999f25e617ec15b1ec
SenvenQi <542559103@qq.com>
2023-02-18 02:33:12 +0800

    .

--- applications/main.c
+++ applications/main.c
@@ -23,8 +23,8 @@ int main(void)
     while (1)
     {
         rt_pin_write(LED3_PIN, PIN_HIGH);
-        rt_thread_mdelay(5000);
+        rt_thread_mdelay(1000);
         rt_pin_write(LED3_PIN, PIN_LOW);
-        rt_thread_mdelay(500);
+        rt_thread_mdelay(1000);
     }
 }
